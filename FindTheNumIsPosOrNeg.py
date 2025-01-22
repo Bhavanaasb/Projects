855d8f3869700600d095b83082b0682c03d6ca92
Sai Bhavana Annadanam <155472478+Bhavanaasb@users.noreply.github.com>
2025-01-22 14:20:43 +0300

    Add files via upload

---
+++ FindTheNumIsPosOrNeg.py
@@ -0,0 +1,11 @@
+#Python program to check if a number is positive, nrgative, or 0
+# +,-,0
+
+
+num=float(input("Enter the number:"))
+if num > 0:
+    print("Positive number")
+elif num==0:
+    print("Zero")
+else:
+    print("Negative")
